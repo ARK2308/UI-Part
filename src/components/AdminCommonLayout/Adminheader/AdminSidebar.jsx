15a79364d784207c092ac7c355eb8560c8eeacc5
ARK2308 <abdulark500@gmail.com>
2024-09-18 03:04:17 +0530

    Admin Verify And Logout Api Integration

--- src/components/AdminCommonLayout/Adminheader/AdminSidebar.jsx
+++ src/components/AdminCommonLayout/Adminheader/AdminSidebar.jsx
@@ -1,111 +1,134 @@
-import React, { useState } from 'react'
-import { NavLink, useLocation, useNavigate } from 'react-router-dom';
-import { Dropdown } from 'react-bootstrap';
+import React, { useEffect, useState } from "react";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import { Dropdown } from "react-bootstrap";
 // import { hamburgurcontext } from '../../../context/ContextProvider';
 // import { useDispatch, useSelector } from 'react-redux';
 // import { AdminLoggedIn, adminLogout } from '../../../redux/slice/adminAuthSlice/AdminSlice';
 import "../../../pages/Admin/admin.css";
+import { AdminLoggedIn, AdminLogout } from "../../../redux/slice/adminAuthSlice/AdminSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const AdminSidebar = ({ children }) => {
+  const { adminLoggedINData } = useSelector((state) => state.Admin);
+  console.log("adminLoggedINData", adminLoggedINData);
 
-    const [ isSidebarActive, setSidebarActive ] = useState(false);
+  const [isSidebarActive, setSidebarActive] = useState(false);
+  const dispatch = useDispatch();
 
-    const toggleSidebar = () => {
-        setSidebarActive(!isSidebarActive);
-    };
+  const navigate = useNavigate();
+  const toggleSidebar = () => {
+    setSidebarActive(!isSidebarActive);
+  };
 
-    const handleLogout = async()=>{}
-    return (
-        <>
-            {/* {
+  // admin verify
+  const AdminVerify = () => {
+    dispatch(AdminLoggedIn());
+  };
+
+
+  useEffect(() => {
+    AdminVerify();
+  }, []);
+
+
+  // logoutfunction
+  const handleLogout = () => {
+    dispatch(AdminLogout())
+      .then((res) => {
+        navigate("/adminlogin");
+      })
+      .catch((error) => {
+        navigate("/adminlogin");
+      });
+  };
+
+  return (
+    <>
+      {/* {
                 location.pathname.split("/")[1] !== "admin" ? "" :
                     ( */}
-                        {/* <>  */}
-                            <div className={`sidebar ${isSidebarActive ? 'active' : ''}`}>
-                                <div className="logo-details" >
-                                    <i class="fa-solid fa-user"></i>
-                                    <span className="logo_name">Abdul Rahman</span>
-                                </div>
-                                <ul className="nav-links">
-                                    <li>
-                                        <NavLink to="/admin/dashboard" className="active">
-                                            <i className="bx bx-grid-alt"></i>
-                                            <span className="links_name">Dashboard</span>
-                                        </NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to="/admin/addproducts">
-                                            <i class="fa-solid fa-truck-fast"></i>
-                                            <span className="links_name">Add Products</span>
-                                        </NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to="/admin/addcategory">
-                                            <i class="fa-solid fa-certificate"></i>
-                                            <span className="links_name">Add category</span>
-                                        </NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to="/admin/products">
-                                            <i class="fa-solid fa-certificate"></i>
-                                            <span className="links_name">Products</span>
-                                        </NavLink>
-                                    </li>
-                                    <li>
-                                        <NavLink to="/admin/orders">
-                                            <i class="fa-solid fa-bag-shopping"></i>
-                                            <span className="links_name">Orders</span>
-                                        </NavLink>
-                                    </li>
-                                    <li>
-                                        <a href="#">
-                                            <i class="fa-solid fa-gear"></i>
-                                            <span className="links_name">settings</span>
-                                        </a>
-                                    </li>
+      {/* <>  */}
+      <div className={`sidebar ${isSidebarActive ? "active" : ""}`}>
+        <div className="logo-details">
+          <i class="fa-solid fa-user"></i>
+          <span className="logo_name">{adminLoggedINData[0]?.name}</span>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <NavLink to="/admin/dashboard" className="active">
+              <i className="bx bx-grid-alt"></i>
+              <span className="links_name">Dashboard</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin/addproducts">
+              <i class="fa-solid fa-truck-fast"></i>
+              <span className="links_name">Add Products</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin/addcategory">
+              <i class="fa-solid fa-certificate"></i>
+              <span className="links_name">Add category</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin/products">
+              <i class="fa-solid fa-certificate"></i>
+              <span className="links_name">Products</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/admin/orders">
+              <i class="fa-solid fa-bag-shopping"></i>
+              <span className="links_name">Orders</span>
+            </NavLink>
+          </li>
+          <li>
+            <a href="#">
+              <i class="fa-solid fa-gear"></i>
+              <span className="links_name">settings</span>
+            </a>
+          </li>
+        </ul>
+      </div>
 
-                                </ul>
-                            </div>
+      <section className="home-section">
+        <nav>
+          <div className="sidebar-button">
+            <i class="fa-solid fa-bars" onClick={toggleSidebar}></i>
+            <span className="dashboard">Dashboard</span>
+          </div>
+          <div className="search-box">
+            <input type="text" placeholder="Search..." />
+            <i className="bx bx-search"></i>
+          </div>
+          <div className="profile-details">
+            <span className="admin_name">{adminLoggedINData[0]?.name}</span>
+            <Dropdown className="text-center">
+              <Dropdown.Toggle className="dropdown_btn" id="dropdown-basic">
+                <img src={adminLoggedINData[0]?.profile} alt="" />
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item onClick={() => handleLogout()}>
+                  <i class="fa-solid fa-right-from-bracket"></i>
+                  &nbsp;&nbsp;&nbsp;Logout
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
+        </nav>
+        <div className="home-content">{children}</div>
+      </section>
 
-                            <section className="home-section">
-                                <nav>
-                                    <div className="sidebar-button">
-                                        <i class="fa-solid fa-bars" onClick={toggleSidebar}></i>
-                                        <span className="dashboard">Dashboard</span>
-                                    </div>
-                                    <div className="search-box">
-                                        <input type="text" placeholder="Search..." />
-                                        <i className="bx bx-search"></i>
-                                    </div>
-                                    <div className="profile-details">
-                                        
-                                        <span className="admin_name">adminLoggedINData[0]?.name</span>
-                                        <Dropdown className='text-center' >
-                                            <Dropdown.Toggle className='dropdown_btn' id="dropdown-basic">
-                                            <img src='adminLoggedINData[0]?.profile' alt="" />
-                                            </Dropdown.Toggle>
-                                            <Dropdown.Menu>
-                                                <Dropdown.Item onClick={()=>handleLogout()}>
-                                                    <i class="fa-solid fa-right-from-bracket"></i>&nbsp;&nbsp;&nbsp;Logout
-                                                </Dropdown.Item>
-                                            </Dropdown.Menu>
-                                        </Dropdown>
-                                    </div>
-                                </nav>
-                                <div className="home-content">
-                                    {children}
-                                </div>
-                            </section>
-
-                        {/* </>
+      {/* </>
                     )
 
 
 
             } */}
+    </>
+  );
+};
 
-        </>
-    )
-}
-
-export default AdminSidebar
+export default AdminSidebar;
--- src/redux/slice/adminAuthSlice/AdminSlice.js
+++ src/redux/slice/adminAuthSlice/AdminSlice.js
@@ -1,23 +1,62 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { AdminLoginApi }  from "../../../Api/AdminApis/adminapi";
+import {
+  AdminLoggedInApi,
+  AdminLoginApi,
+  AdminLogoutApi,
+} from "../../../Api/AdminApis/adminapi";
 import { toast } from "react-hot-toast";
 
-
-
 export const AdminAuthLogin = createAsyncThunk("AdminLogin", async (data) => {
-  try{
+  try {
     const response = await AdminLoginApi(data);
 
-    if(response.status == 200){
-        toast.success("Admin Login Succesfully Done!")
-        localStorage.setItem("admintoken",response.data.token);
-        return response.data
-    }else{
-        toast.error(response.response.data.error);
+    if (response.status == 200) {
+      toast.success("Admin Login Succesfully Done!");
+      localStorage.setItem("admintoken", response.data.token);
+      return response.data;
+    } else {
+      toast.error(response.response.data.error);
     }
-} catch (error) {
+  } catch (error) {
     throw error;
-}
+  }
+});
+
+// Admin LoggedIn Slice
+export const AdminLoggedIn = createAsyncThunk(
+  "AdminLoggedIn",
+  async (thunkApi) => {
+    try {
+      const response = await AdminLoggedInApi();
+      // console.log("response",response);
+      if (response.status == 200) {
+        return response.data;
+      } else {
+        return thunkApi.rejectWithValue("error");
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+);
+
+// Admin Logout Slice
+export const AdminLogout = createAsyncThunk("AdminLogout", async (thunkApi) => {
+  try {
+    const response = await AdminLogoutApi();
+
+    if (response.status == 200) {
+      toast.success("Admin Logout Done");
+      localStorage.removeItem("admintoken");
+      return response.data;
+    } else {
+      toast.success("Admin Logout Done");
+      localStorage.removeItem("admintoken");
+      return thunkApi.rejectWithValue("error");
+    }
+  } catch (error) {
+    throw error;
+  }
 });
 
 // crreate reducer and action
@@ -25,24 +64,52 @@ export const AdminSlice = createSlice({
   name: "AdminSlice",
   initialState: {
     adminlogin: [],
+    adminLoggedINData: [],
+    adminLogoutData: [],
     loading: false,
     error: null,
   },
   extraReducers: (builder) => {
     // Admin Login
-    builder.addCase(AdminAuthLogin.pending, (state) => {
-      state.loading = true;
-    })
-    .addCase(AdminAuthLogin.fulfilled,(state,action)=>{
+    builder
+      .addCase(AdminAuthLogin.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(AdminAuthLogin.fulfilled, (state, action) => {
         state.loading = false;
         state.adminlogin = action.payload;
-    })
-    .addCase(AdminAuthLogin.rejected,(state,action)=>{
+      })
+      .addCase(AdminAuthLogin.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
-    })
+      })
+
+      // Admin Verify
+      .addCase(AdminLoggedIn.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(AdminLoggedIn.fulfilled, (state, action) => {
+        state.loading = false;
+        state.adminLoggedINData = [action.payload];
+      })
+      .addCase(AdminLoggedIn.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+
+      // Admin Logout
+      .addCase(AdminLogout.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(AdminLogout.fulfilled, (state, action) => {
+        state.loading = false;
+        state.adminLogoutData = [action.payload];
+      })
+      .addCase(AdminLogout.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
   },
 });
-
 
 export default AdminSlice.reducer;
