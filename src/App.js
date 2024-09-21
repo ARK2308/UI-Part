29709f87646279887913a79b6b0ef422953ad4cf
ARK2308 <abdulark500@gmail.com>
2024-09-22 01:42:33 +0530

    Protected Routes Completed

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ import ResetPassword from './pages/ForgotPassword/ResetPassword';
 import Shipping from './pages/Shipping/Shipping';
 import Checkout from './pages/Checkout/checkout';
 import UserOrders from './pages/UserOrders/UserOrders';
+import UserProtectedRoutes from './components/Protected/UserProtectedRoutes';
+import AdminProtectedRoutes from './components/Protected/AdminProtectedRoutes';
 
 // Admin Routes Link
 import AdminCommonLayout from './pages/Admin/AdminCommonLayout';
@@ -35,27 +37,27 @@ function App() {
       
         <Routes>
           {/* Admin Routes */}
-          <Route path='/admin/dashboard' element={<AdminCommonLayout ><AdminDashboard /></AdminCommonLayout>} />
-          <Route path='/admin/products' element={<AdminCommonLayout ><AdminProductspage /></AdminCommonLayout>} />
-          <Route path='/admin/addproducts' element={<AdminCommonLayout ><AddProducts /></AdminCommonLayout>} />
-          <Route path='/admin/addcategory' element={<AdminCommonLayout ><AddCategory /></AdminCommonLayout>} />
-          <Route path='/admin/orders' element={<AdminCommonLayout ><Orders /></AdminCommonLayout>} />
+          <Route path='/admin/dashboard' element={<AdminCommonLayout ><AdminProtectedRoutes Components={AdminDashboard} /></AdminCommonLayout>} />
+          <Route path='/admin/products' element={<AdminCommonLayout ><AdminProtectedRoutes Components={AdminProductspage} /></AdminCommonLayout>} />
+          <Route path='/admin/addproducts' element={<AdminCommonLayout ><AdminProtectedRoutes Components={AddCategory} /></AdminCommonLayout>} />
+          <Route path='/admin/addcategory' element={<AdminCommonLayout ><AdminProtectedRoutes Components={AddProducts} /></AdminCommonLayout>} />
+          <Route path='/admin/orders' element={<AdminCommonLayout ><AdminProtectedRoutes Components={Orders} /></AdminCommonLayout>} />
           <Route path='/adminlogin' element={<Layout><AdminLogin /></Layout>} />
 
           {/* User Route */}
           <Route path='/' element={<Layout><Home /></Layout>} />
           <Route path='/products' element={<Layout><ProductsPage /></Layout>} />
           <Route path='/productdetails/:id' element={<Layout><ProductDetailsPage /></Layout>} />
-          <Route path='/carts' element={<Layout><Carts /></Layout>} />
-          <Route path='/userprofile' element={<Layout><UserProfile /></Layout>} />
+          <Route path='/carts' element={<Layout><UserProtectedRoutes Components={Carts} /></Layout>} />
+          <Route path='/userprofile' element={<Layout><UserProtectedRoutes Components={UserProfile} /></Layout>} />
           <Route path="/login" element={<Layout><Login /></Layout>} />
           <Route path="/register" element={<Layout><Register /></Layout>} />
           <Route path="/forgotpassword" element={<Layout><ForgotPassword /></Layout>} />
           <Route path="/resetpassword/:id/:token" element={<Layout><ResetPassword /></Layout>} />
-          <Route path="/shipping" element={<Layout><Shipping /></Layout>} />
-          <Route path="/checkout" element={<Layout><Checkout /></Layout>} />
-          <Route path="/payment" element={<Layout><Payment /></Layout>} />
-          <Route path="/userorders" element={<Layout><UserOrders /></Layout>} />
+          <Route path="/shipping" element={<Layout><UserProtectedRoutes Components={Shipping} /></Layout>} />
+          <Route path="/checkout" element={<Layout><UserProtectedRoutes Components={Checkout} /></Layout>} />
+          <Route path="/payment" element={<Layout><UserProtectedRoutes Components={Payment} /></Layout>} />
+          <Route path="/userorders" element={<Layout><UserProtectedRoutes Components={UserOrders} /></Layout>} />
           <Route path='*' element={<Layout><Error /></Layout>} />
         </Routes>
         <Toaster /> 
